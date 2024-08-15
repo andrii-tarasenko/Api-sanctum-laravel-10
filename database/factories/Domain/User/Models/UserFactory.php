2e1d9615ca8ab8afa541fd9c13235d59a1a89fd6
andrii-tarasenko <tarasenk.andrey@gmail.com>
2024-08-15 11:47:07 +0300

    test feature Autorisation user

--- database/factories/Domain/User/Models/UserFactory.php
+++ database/factories/Domain/User/Models/UserFactory.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Domain\User\Models;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Domain\User\Models\User;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\User\Models\User>
  */
 class UserFactory extends Factory
 {
